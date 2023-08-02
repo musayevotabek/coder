39304a24086fab59b3e4e90a1efc46195a46b256
musayevotabek <141213232+musayevotabek@users.noreply.github.com>
2023-08-02 11:11:07 +0430

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./App.css"
+import "./css.css"
+import Memo from "./Memo";
+function App() {
+  return (
+    <div>
+      {/* <Dars/> */}
+      <Memo />
+    </div>
+  )
+}
+
+export default App
---
+++ Memo.jsx
@@ -0,0 +1,204 @@
+import React from 'react'
+
+function Memo() {
+  return (
+    <div>
+      <div className="navbar">
+        <div className="nav1">
+        <h1>Mercedes-Benz</h1>
+        </div>
+        <div className="nav2">
+        <h3>Home page</h3>
+        <h3>Kompany</h3>
+        <h3>All cars</h3>
+        <h3>Shop</h3>
+        <h3>News</h3>
+        <h3>Contacts</h3>
+        </div>
+      </div>
+      <div className="carusel">
+        <h1>New EQS Suv</h1>
+        <button>Batafsil</button>
+      </div>
+      <div className="wrap">
+        <p>keyingi yangiliklar</p>
+        <h1>Tezyurar mashinalarga aksiya</h1>
+      <div className="cards">
+        <div className="car">
+          <img src="https://images.unsplash.com/photo-1502877338535-766e1452684a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y2FyfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" alt="" />
+          <div className="carsText">
+            <div className="descs">
+            <div className="desc"><i className="fa-solid fa-calendar-days"></i><p>17.Okt.2022</p></div>
+            <div className="desc">
+              <i className='fa-solid fa-eye'></i>
+              <p>32145</p>
+            </div>
+            <div className="desc">
+              <i className='fa fa-share-nodes'></i>
+            </div>
+            </div>
+            <div className="text">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+              <p>Batafsil</p>
+            </div>
+          </div>
+        </div>
+        <div className="car">
+          <img src="https://images.unsplash.com/photo-1502877338535-766e1452684a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y2FyfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" alt="" />
+          <div className="carsText">
+            <div className="descs">
+            <div className="desc"><i className="fa-solid fa-calendar-days"></i><p>17.Okt.2022</p></div>
+            <div className="desc">
+              <i className='fa-solid fa-eye'></i>
+              <p>32145</p>
+            </div>
+            <div className="desc">
+              <i className='fa fa-share-nodes'></i>
+            </div>
+            </div>
+            <div className="text">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+              <p>Batafsil</p>
+            </div>
+          </div>
+        </div>
+        <div className="car">
+          <img src="https://images.unsplash.com/photo-1502877338535-766e1452684a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y2FyfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" alt="" />
+          <div className="carsText">
+            <div className="descs">
+            <div className="desc"><i className="fa-solid fa-calendar-days"></i><p>17.Okt.2022</p></div>
+            <div className="desc">
+              <i className='fa-solid fa-eye'></i>
+              <p>32145</p>
+            </div>
+            <div className="desc">
+              <i className='fa fa-share-nodes'></i>
+            </div>
+            </div>
+            <div className="text">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+              <p>Batafsil</p>
+            </div>
+          </div>
+        </div>
+        <div className="car">
+          <img src="https://images.unsplash.com/photo-1502877338535-766e1452684a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y2FyfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" alt="" />
+          <div className="carsText">
+            <div className="descs">
+            <div className="desc"><i className="fa-solid fa-calendar-days"></i><p>17.Okt.2022</p></div>
+            <div className="desc">
+              <i className='fa-solid fa-eye'></i>
+              <p>32145</p>
+            </div>
+            <div className="desc">
+              <i className='fa fa-share-nodes'></i>
+            </div>
+            </div>
+            <div className="text">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+              <p>Batafsil</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
+      <div className="positi">
+        <div className="po">
+          <p>Company</p>
+          <h1>Lorem ipsum, dolor sit amet consectetur adipisicing.</h1>
+          <ul>
+            <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda ipsam inventore nam quisquam. Laborum, ducimus eum? Consequatur accusantium odit minus! Repudiandae, corrupti ullam dolores hic quasi ipsam vel temporibus natus!</li>
+            <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda ipsam inventore nam quisquam. Laborum, ducimus eum? Consequatur accusantium odit minus! Repudiandae, corrupti ullam dolores hic quasi ipsam vel temporibus natus!</li> 
+            <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda ipsam inventore nam quisquam. Laborum, ducimus eum? Consequatur accusantium odit minus! Repudiandae, corrupti ullam dolores hic quasi ipsam vel temporibus natus!</li> 
+          </ul>
+        </div>
+      </div>
+      <div className="content">
+      <p>keyingi yangiliklar</p>
+      <h1>Tezyurar mashinalarga aksiya</h1>
+      <div className="big">
+        <div className="divcha">
+          <img src="https://images.unsplash.com/photo-1502877338535-766e1452684a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y2FyfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" alt="" />
+          <p>G Klass</p>
+          <p>Merscedenz-Benz EQC</p>
+        </div>
+        <div className="divcha">
+          <img src="https://images.unsplash.com/photo-1502877338535-766e1452684a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y2FyfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" alt="" />
+          <p>G Klass</p>
+          <p>Merscedenz-Benz EQC</p>
+        </div>
+        <div className="divcha">
+          <img src="https://images.unsplash.com/photo-1502877338535-766e1452684a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y2FyfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" alt="" />
+          <p>G Klass</p>
+          <p>Merscedenz-Benz EQC</p>
+        </div>
+        <div className="divcha">
+          <img src="https://images.unsplash.com/photo-1502877338535-766e1452684a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y2FyfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" alt="" />
+          <p>G Klass</p>
+          <p>Merscedenz-Benz EQC</p>
+        </div>
+        <div className="divcha">
+          <img src="https://images.unsplash.com/photo-1502877338535-766e1452684a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y2FyfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" alt="" />
+          <p>G Klass</p>
+          <p>Merscedenz-Benz EQC</p>
+        </div>
+      </div>
+      </div>
+      <div className="future">
+      <div className="sarlavha">
+      <p>keyingi yangiliklar</p>
+      <h1>Tezyurar mashinalarga aksiya</h1>
+      </div>
+      <div className="yozuv">
+        <h4>All cars</h4>
+        <h4><u>A Class</u></h4>
+        <h4>C Class</h4>
+        <h4>E Class</h4>
+        <h4>G Class</h4>
+      </div>
+      </div>
+      <div className="feeter">
+        <div className="top">
+          <div className="t-divs">
+            ALOQA <br />
+            Lorem ipsum dolor, sit amet consectetur <br /> adipisicing.
+          </div>
+          <div className="t-divs">
+            Pul <br />
+            Lorem ipsum dolor sit amet consectetur <br /> adipisicing.
+          </div>
+          <div className="t-divs">
+            Sifat <br />
+            Lorem ipsum dolor sit amet consectetur <br /> adipisicing.
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="b-divs">
+            Odamlar <br />
+            Lorem ipsum dolor sit amet consectetur <br /> adipisicing.
+          </div>
+          <div className="b-divs">
+            Buyurtma <br />
+            Lorem ipsum dolor sit amet consectetur <br /> adipisicing.
+          </div>
+          <div className="b-divs">
+            Yetkazish <br />
+            Lorem ipsum dolor sit amet consectetur <br /> adipisicing.
+          </div>
+        </div>
+      </div>
+      <div className="grow">
+        <div className="single">
+          <div className="row">
+          </div>
+          <div className="row">
+          </div>
+          <div className="row">
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Memo
